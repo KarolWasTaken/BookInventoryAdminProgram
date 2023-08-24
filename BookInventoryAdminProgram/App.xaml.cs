546003048b784b3cfcfa8e3e02150dd2a15a4785
Karol <karol@cockandball.es>
2023-08-24 16:20:50 +0100

    Add project files.

---
+++ BookInventoryAdminProgram/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace BookInventoryAdminProgram
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            // Debug to check if connection to server is working. Throw if not.
+            /*MessageBox.Show($"{Helper.CnnVal()}"); debug
+
+            using (SqlConnection conn = new SqlConnection(Helper.CnnVal()))
+            {
+                conn.Open(); // throws if invalid
+            }*/
+
+
+
+
+
+
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ BookInventoryAdminProgram/Helper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace BookInventoryAdminProgram
+{
+    public static class Helper
+    {
+
+        public static string? CnnVal ()
+        {
+            IConfigurationRoot config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+            Settings? settings = config.GetRequiredSection("Settings").Get<Settings>();
+            return settings?.ConnectionString;
+        }
+    }
+}
