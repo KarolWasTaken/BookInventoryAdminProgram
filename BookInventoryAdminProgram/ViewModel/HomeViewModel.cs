71806018f06afd2e77fb58820ce2e8ff8e31e812
Karol <karol@cockandball.es>
2023-10-15 12:36:07 +0100

    added a lot of null handling in homeviewmodel initialisation

--- BookInventoryAdminProgram/ViewModel/HomeViewModel.cs
+++ BookInventoryAdminProgram/ViewModel/HomeViewModel.cs
@@ -12,6 +12,7 @@ using System.Transactions;
 using System.Windows;
 using System.Windows.Markup;
 using System.Windows.Media.Imaging;
+using static BookInventoryAdminProgram.Model.PopularityCalculator;
 using static BookInventoryAdminProgram.Stores.DatabaseStore;
 
 namespace BookInventoryAdminProgram.ViewModel
@@ -59,6 +60,22 @@ namespace BookInventoryAdminProgram.ViewModel
         {
             // this prolly breaks mvvm principles but idc
             var bestSeller = pc.GetMostPopularBook(new DateTime(2023, 9, 12), new DateTime(2023, 10, 12));
+            if(bestSeller == null)
+            {
+                PopularBookCover = GetNoCoverImage();
+                PopularBookInfo = new Dictionary<string, object>()
+                {
+                    { "Title", "No book found"},
+                    { "Authors", string.Empty},
+                    { "Genres",  string.Empty},
+                    { "ReleaseDate",  string.Empty},       // empty strings to prevent binding errors.
+                    { "Publisher",  string.Empty},
+                    { "ISBN",  string.Empty},
+                    { "Sales", string.Empty}
+                };
+                return;
+            }
+            
             BookInfo popularBook = (BookInfo)bestSeller[0];
             PopularBookInfo = new Dictionary<string, object>()
             {
@@ -80,25 +97,35 @@ namespace BookInventoryAdminProgram.ViewModel
             if (imageBytes == null)
             {
 
-                BitmapImage defaultNoCoverImageBitmap = new BitmapImage(new Uri("pack://application:,,,/Resources/BookImages/NoCoverDefaultTwo.png"));
-                byte[] defaultNoCoverImage = BitmapImageToByteArrayConverter.Convert(defaultNoCoverImageBitmap);
-                PopularBookCover = defaultNoCoverImage;
+                PopularBookCover = GetNoCoverImage();
             }
             else { PopularBookCover = imageBytes; }
             // Use the imageBytes to display the image or save it to a file.
         }
         private void InitialiseNotiflicationPanel()
         {
-            var listOfMostPopularProperties = pc.GetAllPopulatities();
+            Dictionary<string, List<SQLPopularity>> listOfMostPopularProperties = pc.GetAllPopulatities();
             DatabaseOperations dbo = new DatabaseOperations();
 
-
             NotiflicationPanelMessage = new Dictionary<string, string>()
+            {
+                { "Genre", string.Empty},
+                { "Author", string.Empty},
+                { "Publisher", string.Empty}
+            };
+
+            foreach (string key in listOfMostPopularProperties.Keys )
+            {
+                if (listOfMostPopularProperties[key] != null && key != "Book")
+                    NotiflicationPanelMessage[key] = $"• {dbo.GetPropertyByID(key, listOfMostPopularProperties[key][0].ID)}\n• {dbo.GetPropertyByID(key, listOfMostPopularProperties[key][1].ID)}\n• {dbo.GetPropertyByID(key, listOfMostPopularProperties[key][2].ID)}";
+            }
+
+            /*NotiflicationPanelMessage = new Dictionary<string, string>()
             {
                 { "Genre", $"• {dbo.GetPropertyByID("Genre",listOfMostPopularProperties["Genre"][0].ID)}\n• {dbo.GetPropertyByID("Genre",listOfMostPopularProperties["Genre"][1].ID)}\n• {dbo.GetPropertyByID("Genre",listOfMostPopularProperties["Genre"][2].ID)}"},
                 { "Author", $"• {dbo.GetPropertyByID("Author",listOfMostPopularProperties["Author"][0].ID)}\n• {dbo.GetPropertyByID("Author",listOfMostPopularProperties["Author"][1].ID)}\n• {dbo.GetPropertyByID("Author",listOfMostPopularProperties["Author"][2].ID)}"},
                 { "Publisher", $"• {dbo.GetPropertyByID("Publisher",listOfMostPopularProperties["Publisher"][0].ID)}\n• {dbo.GetPropertyByID("Publisher", listOfMostPopularProperties["Publisher"][1].ID)}\n• {dbo.GetPropertyByID("Publisher",listOfMostPopularProperties["Publisher"][2].ID)}"}
-            };
+            };*/
 
 
             var booksLowInStock = dbo.GetBooksLowInStock();
@@ -112,7 +139,21 @@ namespace BookInventoryAdminProgram.ViewModel
                 LowInStockMessage += "\nConsider restocking!";
                 MoreInfoRestock["Stock"] = true;
             }
-            List<BookInfo> genresWithBooksLowInStock = dbo.GetBooksLowInStock("Genre", new List<string>()
+            foreach (string property in new List<string>() {"Genre", "Author" })
+            {
+                if(listOfMostPopularProperties[property] != null)
+                {
+                    List<BookInfo> propertyWithBooksLowInStock = dbo.GetBooksLowInStock(property, new List<string>()
+                    {
+                        dbo.GetPropertyByID(property,listOfMostPopularProperties[property][0].ID),
+                        dbo.GetPropertyByID(property,listOfMostPopularProperties[property][1].ID),
+                        dbo.GetPropertyByID(property,listOfMostPopularProperties[property][2].ID)
+
+                    });
+                    UpdateCategoryInfo(propertyWithBooksLowInStock, property);
+                }
+            }
+            /*List<BookInfo> genresWithBooksLowInStock = dbo.GetBooksLowInStock("Genre", new List<string>()
             {
                 dbo.GetPropertyByID("Genre",listOfMostPopularProperties["Genre"][0].ID),
                 dbo.GetPropertyByID("Genre",listOfMostPopularProperties["Genre"][1].ID),
@@ -123,11 +164,17 @@ namespace BookInventoryAdminProgram.ViewModel
                 dbo.GetPropertyByID("Author",listOfMostPopularProperties["Author"][0].ID),
                 dbo.GetPropertyByID("Author",listOfMostPopularProperties["Author"][1].ID),
                 dbo.GetPropertyByID("Author",listOfMostPopularProperties["Author"][2].ID)
-            });
+            });*/
 
-            UpdateCategoryInfo(genresWithBooksLowInStock, "Genre");
-            UpdateCategoryInfo(authorsWithBooksLowInStock, "Author");
+            //UpdateCategoryInfo(genresWithBooksLowInStock, "Genre");
+            //UpdateCategoryInfo(authorsWithBooksLowInStock, "Author");
         }
+
+
+
+
+
+
         private void UpdateCategoryInfo(List<BookInfo> booksLowInStock, string category)
         {
             if (booksLowInStock != null && booksLowInStock.Count > 0)
@@ -136,6 +183,11 @@ namespace BookInventoryAdminProgram.ViewModel
                 MoreInfoRestock[category] = true;
             }
         }
-
+        private byte[] GetNoCoverImage()
+        {
+            BitmapImage defaultNoCoverImageBitmap = new BitmapImage(new Uri("pack://application:,,,/Resources/BookImages/NoCoverDefault.png"));
+            byte[] defaultNoCoverImage = BitmapImageToByteArrayConverter.Convert(defaultNoCoverImageBitmap);
+            return defaultNoCoverImage;
+        }
     }
 }
